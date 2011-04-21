36521a7c466b4a6185f9c5985ec9daaa93758050
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2011-04-22 00:22:36 +0900

    Make it pretty

--- extension/chrodic.js
+++ extension/chrodic.js
@@ -1,8 +1,9 @@
 console.log('chrodic loaded!');
 
-function getWord(event) {
+function getWord(event, n) {
   var range = document.caretRangeFromPoint(event.clientX, event.clientY);
   if (!range.startContainer.nodeValue) return '';
+  // Expand left.
   while (range.startOffset > 0) {
     range.setStart(range.startContainer, range.startOffset - 1);
     if (!/^[a-zA-Z0-9]/.test(range.toString())) {
@@ -10,12 +11,33 @@ function getWord(event) {
       break;
     }
   }
+  // Expand right.
   while (range.endOffset < range.endContainer.nodeValue.length) {
     range.setEnd(range.endContainer, range.endOffset + 1);
     if (!/[a-zA-Z0-9]$/.test(range.toString())) {
       range.setEnd(range.endContainer, range.endOffset - 1);
       break;
     }
+  }
+  for (var i = 1; i < n; ++i) {
+    while (range.endOffset < range.endContainer.nodeValue.length) {
+      range.setEnd(range.endContainer, range.endOffset + 1);
+      if (/[a-zA-Z0-9]$/.test(range.toString())) {
+        range.setEnd(range.endContainer, range.endOffset - 1);
+        break;
+      }
+    }
+    while (range.endOffset < range.endContainer.nodeValue.length) {
+      range.setEnd(range.endContainer, range.endOffset + 1);
+      if (!/[a-zA-Z0-9]$/.test(range.toString())) {
+        range.setEnd(range.endContainer, range.endOffset - 1);
+        break;
+      }
+    }
+  }
+  while (range.endOffset > range.startOffset &&
+         !/[a-zA-Z0-9]$/.test(range.toString())) {
+    range.setEnd(range.endContainer, range.endOffset - 1);
   }
   return range.toString().toLowerCase();
 }
@@ -36,11 +58,41 @@ with (translation_box.style) {
 document.body.appendChild(translation_box);
 
 document.addEventListener('click', function(event) {
-  if (event.metaKey) {
+  if (event.shiftKey) {
     translation_box.style.display =
         translation_box.style.display == 'block' ? 'none' : 'block';
   }
 }, false);
+
+var REWRITE_RULES = [
+  [/s$/, ''],
+  [/es$/, 'e'],
+  [/es$/, ''],
+  [/ies$/, 'y'],
+  [/ing$/, ''],
+  [/ing$/, 'e'],
+  [/ed$/, 'e'],
+  [/ed$/, ''],
+  [/(.)$1ing$/, ''],
+  [/ied$/, 'y']
+];
+
+function translate(word) {
+  chrome.extension.sendRequest(
+    {'action' : 'translateWord', 'word' : word},
+    function (translation) {
+      var match = translation.match(/<→(.*)>/);
+      if (match) {
+        translate(match[1]);
+//        return;
+      }
+      if (translation != '') {
+        translation_box.innerHTML +=
+        ('<span style="font-size:14px;">' + word + '</span>\n<div style="margin:5px;">' + translation + '</div>').
+            replace(/\n/g, '<br />');
+      }
+    });
+}
 
 document.addEventListener('mousemove', function(event) {
   // Redraw translation box.
@@ -54,35 +106,23 @@ document.addEventListener('mousemove', function(event) {
     top = box_top + 'px';
   }
 
-  var word = getWord(event);
+  var word = getWord(event, 1);
   if (word == previous_word) return;
   previous_word = word;
   translation_box.innerHTML = '';
 
-  function translate(w) {
-    chrome.extension.sendRequest(
-      {'action' : 'translateWord', 'word' : w},
-      function (translation) {
-        //console.log(w + " " + translation);
-        if (translation != '') {
-          if (translation_box.innerHTML != '') {
-            translation_box.innerHTML += '<br />';
-          }
-          translation_box.innerHTML +=
-          ('<span style="font-size:14px">' + w + '</span>\n' + translation).
-            replace(/\n/g, '<br />');
-        }
-      });
+  //if (translation_box.style.display == 'none') return;
+  translate(word);
+  for (var i = 0; i < REWRITE_RULES.length; ++i) {
+    if (REWRITE_RULES[i][0].test(word)) {
+      translate(word.replace(REWRITE_RULES[i][0], REWRITE_RULES[i][1]));
+    }
   }
 
-  translate(word);
-  if (/s$/.test(word)) translate(word.replace(/s$/, ''));
-  if (/es$/.test(word)) translate(word.replace(/s$/, ''));
-  if (/ies$/.test(word)) translate(word.replace(/ies$/, 'y'));
-  if (/ing$/.test(word)) translate(word.replace(/ing$/, ''));
-  if (/ing$/.test(word)) translate(word.replace(/ing$/, 'e'));
-  if (/ed$/.test(word)) translate(word.replace(/ed$/, 'e'));
-  if (/ed$/.test(word)) translate(word.replace(/ed$/, ''));
-  if (/.{2}ing$/.test(word)) translate(word.replace(/.{2}ing$/, ''));
-  if (/ied$/.test(word)) translate(word.replace(/ied$/, 'y'));
+  for (var i = 2; i <= 5; ++i) { 
+    var new_word = getWord(event, i);
+    if (new_word == word) break;
+    word = new_word;
+    translate(word);
+  }
 }, false);
