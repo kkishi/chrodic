0fa0cf8da1b93660103efe1af101b674923f4a03
Keisuke Kishimoto <keisuke@google.com>
2013-11-06 17:44:25 +0900

    Handle onMessages when db is null.

--- extension/background.js
+++ extension/background.js
@@ -1,15 +1,19 @@
+var pending = [];
+
 // Open DB.
 var db;
 indexedDB.open("dict", 1).onsuccess = function(e) {
   db = e.target.result;
+  for (var i = 0; i < pending.length; ++i) {
+    pending[i]();
+  }
+  pending = [];
 };
 
-function onMessage(request, sender, callback) {
-  if (request.action != 'translateWord') return false;
-
+function respond(word, callback) {
   // Lookup the requested word from DB.
   var trans = db.transaction(["word"], "readonly");
-  trans.objectStore("word").openCursor(request.word).onsuccess = function(e) {
+  trans.objectStore("word").openCursor(word).onsuccess = function(e) {
     var result = e.target.result;
     if (result) {
       callback(result.value.value);
@@ -17,7 +21,16 @@ function onMessage(request, sender, callback) {
       callback(null);
     }
   };
+}
 
+function onMessage(request, sender, callback) {
+  if (request.action != 'translateWord') return false;
+  if (db == null) {
+    pending.push(function() { respond(request.word, callback); });
+  } else {
+    respond(request.word, callback);
+  }
   return true;
 }
+
 chrome.runtime.onMessage.addListener(onMessage);
